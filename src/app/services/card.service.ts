1bdb19dc200559e47741ea511d3bb9e3503bba2b
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-02 14:01:34 +0300

    Reload bug fix.

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -6,6 +6,8 @@ import { Card } from '../models/card';
 })
 export class CardService {
   private cards: Card[] = JSON.parse(localStorage.getItem('cards') || '[]');
+
+  private filterQuery: string = '';
   public filteredCards: Card[] = this.cards;
 
   constructor() {}
@@ -17,31 +19,37 @@ export class CardService {
   Add(card: Card) {
     card.id = new Date().getTime();
     this.cards.push(card);
-    localStorage.setItem('cards', JSON.stringify(this.cards));
+    this.Reload();
   }
 
   Remove(id: number) {
     this.cards = this.cards.filter((item) => item.id !== id);
-    localStorage.setItem('cards', JSON.stringify(this.cards));
+    this.Reload();
   }
 
   Update(id: number, card: Card) {
     this.Remove(id);
     card.id = id;
     this.cards.push(card);
-    localStorage.setItem('cards', JSON.stringify(this.cards));
+    this.Reload();
   }
 
   Filter(query: string) {
-    query = query.toLowerCase();
+    this.filterQuery = query.toLowerCase();
     this.filteredCards = this.cards.filter((card) => {
       return (
-        card.name.toLowerCase().indexOf(query) > -1 ||
-        card.title.toLowerCase().indexOf(query) > -1 ||
-        card.phone.toLowerCase().indexOf(query) > -1 ||
-        card.email.toLowerCase().indexOf(query) > -1 ||
-        card.address.toLowerCase().indexOf(query) > -1
+        card.name.toLowerCase().indexOf(this.filterQuery) > -1 ||
+        card.title.toLowerCase().indexOf(this.filterQuery) > -1 ||
+        card.phone.toLowerCase().indexOf(this.filterQuery) > -1 ||
+        card.email.toLowerCase().indexOf(this.filterQuery) > -1 ||
+        card.address.toLowerCase().indexOf(this.filterQuery) > -1
       );
     });
+  }
+
+  private Reload() {
+    localStorage.setItem('cards', JSON.stringify(this.cards));
+    this.filteredCards = this.cards;
+    this.Filter(this.filterQuery);
   }
 }
