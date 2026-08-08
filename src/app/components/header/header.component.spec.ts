04a89996e962d74874b5193bc3bc8038cb90b3f4
Necdet UYGUR <necdet.uygur@gmail.com>
2026-08-08 15:21:48 +0300

    Add Header component and app shell layout

---
+++ src/app/components/header/header.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+    }).compileComponents();
+  });
+
+  it('should create', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+});
